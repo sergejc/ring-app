f8ada5a033dcb55f620ad6d44bc139b99c238e83
Sergej Charskij <sergej.charskij@gmail.com>
2017-05-29 22:50:14 +0100

    added compojure routes

--- src/ring_app/core.clj
+++ src/ring_app/core.clj
@@ -1,27 +1,21 @@
 (ns ring-app.core
   (:require [ring.adapter.jetty :as jetty]
+            [compojure.core :as compojure]
             [ring.util.http-response :as response]
-            [ring.middleware.reload :refer [wrap-reload]]
-            [ring.middleware.format :refer [wrap-restful-format]]))
+            [ring.middleware.reload :refer [wrap-reload]]))
+
+(defn response-handler [request]
+  (response/ok
+    (str "<html><body> your IP is: "
+         (:remote-addr request)
+         "</body></html>")))
 
 (defn handler [request]
-  (response/ok {:result (-> request :params :id)}))
-
-
-(defn wrap-nocache [handler]
-  (fn [request]
-  (-> request
-      handler
-      (assoc-in [:headers "Pragma"] "no-cache"))))
-
-(defn wrap-formats [handler]
-  (wrap-restful-format
-    handler
-    {:formats [:json-kw :transit-json :transit-msgpack]}))
-
+  (compojure/routes
+    (compojure/GET "/" request response-handler)))
 
 (defn -main []
   (jetty/run-jetty
-    (-> #'handler wrap-nocache wrap-reload wrap-formats)
+    handler
     {:port 3000
      :join? false}))
