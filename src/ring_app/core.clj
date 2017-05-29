8c017edf43ee98d34e3e64a375c1622648749b4e
Sergej Charskij <sergej.charskij@gmail.com>
2017-05-29 22:42:32 +0100

    added json handler

--- src/ring_app/core.clj
+++ src/ring_app/core.clj
@@ -1,16 +1,27 @@
 (ns ring-app.core
-  (:require [ring.adapter.jetty :as jetty]))
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.util.http-response :as response]
+            [ring.middleware.reload :refer [wrap-reload]]
+            [ring.middleware.format :refer [wrap-restful-format]]))
 
-(defn handler [request-map]
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body (str "<html><body> your IP is: "
-              (:remote-addr request-map)
-              "</body></html>")})
+(defn handler [request]
+  (response/ok {:result (-> request :params :id)}))
+
+
+(defn wrap-nocache [handler]
+  (fn [request]
+  (-> request
+      handler
+      (assoc-in [:headers "Pragma"] "no-cache"))))
+
+(defn wrap-formats [handler]
+  (wrap-restful-format
+    handler
+    {:formats [:json-kw :transit-json :transit-msgpack]}))
 
 
 (defn -main []
   (jetty/run-jetty
-  handler
-  {:port 3000
-   :join? false}))
+    (-> #'handler wrap-nocache wrap-reload wrap-formats)
+    {:port 3000
+     :join? false}))
